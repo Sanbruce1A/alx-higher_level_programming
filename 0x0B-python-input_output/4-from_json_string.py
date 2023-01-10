84b7c553388c0ff4f87ac708766e521b1a889228
Sanbruce1A <saniimoleayo@gmail.com>
2023-01-10 12:36:45 -0800

    Task all

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a JSON-to-object function."""
+
+import json
+
+
+def from_json_string(my_str):
+
+    """returns an object represented by a JSON string"""
+
+    return json.loads(my_str)
